320c5d336205f6c2e7826b36767dbd3a67f0bb6b
TianZhou2718 <TianZhou2718@users.noreply.github.com>
2025-08-21 05:50:09 +0000

    deploy: 4a8edec13a38548e26e815a17f983ebdaae82e9b

--- api/analytics/total-visitors.js
+++ api/analytics/total-visitors.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   // Set CORS headers for all responses
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cache-Control, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cache-Control, Accept, Authorization');
 
   // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
